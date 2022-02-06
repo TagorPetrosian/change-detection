5c2d398fd69b365e4d8e2b481a5f93b3bfe329cd
Tagor Petrosian <p.tagor@gmail.com>
2022-02-06 16:16:55 +0200

    first commt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'change-detection';
+  /**
+   *
+   */
+  constructor(appRef: ApplicationRef) {
+    const originalTick = appRef.tick;
+    appRef.tick = function() {
+      const returnedValue = originalTick.apply(this);
+      console.log('change detection');
+      return returnedValue;
+    }
+    
+  }
+
+  public tasks = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'delectus aut autem',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'fugiat veniam minus',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 4,
+      title: 'et porro tempora',
+      completed: true,
+    },
+  ]
+
+  // ng on change prev value and new value
+
+  onClick() :void{
+    this.tasks.forEach(task => task.title = `title ${task.id}`)
+  }
 }
